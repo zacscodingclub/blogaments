e3b83893f138ba62dbc5fec85f161a3239e8b6d7
zacscodingclub <zbaston@gmail.com>
2016-09-23 10:58:05 -0400

    built flash messages helper to handle display. add helper to layout.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,7 +11,8 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
 
     if @article.save
-      redirect :show
+      flash[:notice] = "Article was created"
+      redirect_to article_path(@article)
     else
       render :new
     end
