65a93f35d8e24e1bd386407cca63b9001faf3160
zacscodingclub <zbaston@gmail.com>
2016-09-23 11:29:19 -0400

    add Article#destroy action and link in show page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:new, :show, :edit, :update, :destroy]
   def index
     @articles = Article.all
   end
@@ -19,16 +20,14 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
+
   end
 
   def edit
-    @article = Article.find(params[:id])
+
   end
 
   def update
-    @article = Article.find(params[:id])
-
     if @article.update(article_params)
       flash[:notice] = "Article was updated"
       redirect_to @article
@@ -38,11 +37,17 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-
+    @article.destroy
+    flash[:notice] = "Article was deleted"
+    redirect_to articles_path
   end
 
   private
     def article_params
       params.require(:article).permit(:title, :description)
+    end
+
+    def set_article
+      @article ||= Article.find(params[:id])
     end
 end
