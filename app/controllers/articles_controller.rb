767b161ccd054bdeb80bd61b342d207ddcc6b098
zacscodingclub <zbaston@gmail.com>
2016-09-24 17:33:54 -0400

    update application template, including new navbar partial and styles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: [:new, :show, :edit, :update, :destroy]
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
   def index
     @articles = Article.all
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  root 'articles#index'
+  root 'pages#home'
   resources :articles
 end
