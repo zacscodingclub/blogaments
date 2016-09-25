a2a5600c7394bb764e383630df54d839d25957f2
zacscodingclub <zbaston@gmail.com>
2016-09-25 12:16:09 -0400

    add User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[^@\s]+@([^@.\s]+\.)+[^@.\s]+\z/
+  
   has_many :comments
+  validates :username, presence: true, uniqueness: { case_sensitive: false },
+                       length: { minimum: 3, maximum: 25 }
+  validates :email, presence: true, uniqueness: { case_sensitive: false },
+                    format: { with: VALID_EMAIL_REGEX,
+                           message: "is not a valid" }
 end
