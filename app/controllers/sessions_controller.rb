2a2f1e276f0f57dbb0329ae0a94c7132298eafe1
zacscodingclub <zbaston@gmail.com>
2016-09-26 08:13:22 -0400

    build out login and logout functionality, add links to those actions in navbar

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,22 @@ class SessionsController < ApplicationController
 
   def create
     #actual login
-    binding.pry
+    @user = User.find_by(email: session_params[:email].downcase)
+
+    if @user && @user.authenticate(session_params[:password])
+      session[:user_id] = @user.id
+      flash[:success] = @user.username + " has successfully logged in."
+      redirect_to articles_path
+    else
+      flash.now[:danger] = "Could not log you in.  Please try again."
+      render :new
+    end
   end
 
   def destroy
-    #logout
+    session.clear
+    flash[:success] = "You have logged out."
+    redirect_to root_path
   end
 
   private
