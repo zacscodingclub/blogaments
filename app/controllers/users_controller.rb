58d4f208551c273e7a1c159e62e0a876d3ad0b6b
zacscodingclub <zbaston@gmail.com>
2016-09-26 08:49:15 -0400

    restrict actions at the controller level and add one more restriction i forgot in User#show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-
+  before_action :require_same_user, only: [:edit, :update, :destroy]
   # GET /users
   # GET /users.json
   def index
@@ -29,6 +29,7 @@ class UsersController < ApplicationController
 
 
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to Blogaments #{@user.username}!"
       redirect_to articles_path
     else
@@ -67,5 +68,12 @@ class UsersController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       params.require(:user).permit(:username, :email, :password)
+    end
+
+    def require_same_user
+      if current_user != @user
+        flash[:danger] = "You do not have permission for that action."
+        redirect_to root_path
+      end
     end
 end
