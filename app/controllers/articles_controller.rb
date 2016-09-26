d4334c41eb23dd3a5bc942f13087475c5aeee31c
zacscodingclub <zbaston@gmail.com>
2016-09-26 08:49:57 -0400

    set article user to current user

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,7 +13,8 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-
+    @article.user = current_user
+    
     if @article.save
       flash[:success] = "Article was created"
       redirect_to @article
