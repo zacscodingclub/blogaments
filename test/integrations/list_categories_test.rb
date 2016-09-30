31c06b53011cbf79f9c66634fb4375c98064b0c1
zacscodingclub <zbaston@gmail.com>
2016-09-30 16:12:37 -0400

    finish test for listing Categories#index page.  add link to Categories in navbar.

--- test/integrations/list_categories_test.rb
+++ test/integrations/list_categories_test.rb
@@ -2,7 +2,15 @@ require 'test_helper'
 
 class ListCategoriesTest < ActionDispatch::IntegrationTest
   def setup
-    @category_one = category(:one)
-    @category_two = category(:two)
+    @category_one = categories(:one)
+    @category_two = categories(:two)
+  end
+
+  test "should show categories listing" do
+    get categories_path
+    assert_template 'categories/index'
+
+    assert_select "a[href=?]", category_path(@category_one), text: @category_one.name
+    assert_select "a[href=?]", category_path(@category_two), text: @category_two.name
   end
 end
