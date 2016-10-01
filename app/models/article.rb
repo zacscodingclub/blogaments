55e3eb5a48e6f0f1b0f8b1d470bc17eda88f83f1
zacscodingclub <zbaston@gmail.com>
2016-10-01 11:55:37 -0400

    migrate ArticleCategory join table and setup associations in the models

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,8 @@
 class Article < ApplicationRecord
   belongs_to :user
+  has_many :article_categories
+  has_many :categories, through: :article_categories
+  
   validates :title, presence: true, length: { minimum: 5 }
   validates :description, presence: true, length: { minimum: 100 }
   validates :user_id, presence: true
